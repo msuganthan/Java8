2db9c4030686fdd4c93f027da16fd07469d8bde7
msuganthan <suganthanchandrika@gmail.com>
2016-03-01 02:06:22 +0530

    Optional

---
+++ Java08Prj01/src/Optional/Optional01.java
@@ -0,0 +1,73 @@
+package Optional;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by sugan_000 on 3/1/2016.
+ */
+public class Optional01 {
+
+    public static  void main(String[] args) {
+        Company company = getCompanyData();
+        for(Department department : company.getDepartment()) {
+            System.out.println(department.getName() + " "+department.getManager().getName());
+        }
+    }
+
+    public static Company getCompanyData() {
+        Manager manager = new Manager("Sugathan");
+        Department department = new Department("department1");
+        department.setManager(manager);
+
+        Department department1 = new Department("department1");
+
+        Company company = new Company((Arrays.asList(department, department1)));
+        return company;
+    }
+}
+
+class Company {
+    List<Department> departments;
+
+    public Company(List<Department> departments) {
+        this.departments = departments;
+    }
+    public List<Department> getDepartment() {
+        return departments;
+    }
+
+}
+
+class Department {
+    String name;
+    Manager manager;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+}
+class Manager {
+    String name;
+
+    public Manager(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
